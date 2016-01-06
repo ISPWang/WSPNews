87660eb5fff8e533a238c62c0f864d3464b04693
ISPWang <sanpeng123@126.com>
2016-01-06 16:27:18 +0800

    Initial Commit

---
+++ WSPNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSPNews
+//
+//  Created by auto on 16/1/6.
+//  Copyright © 2016年 auto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
